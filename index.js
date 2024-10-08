2d7497025d22c91be6bfe649e5266ebd514e7139
thornm <atmaystar@gmail.com>
2024-10-08 13:35:13 -0400

    commit strings lab review

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = "Tom Hardaway";
+
+const firstInitial = currentUser.charAt(0);
+
+const lastName = currentUser.slice(4, 12)
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+const shortGreeting = `Welcome, ${firstInitial} ${lastName}!`
